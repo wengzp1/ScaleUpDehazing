378d152f2e5dcd18f94b2b984964d7d36cb82c22
wengzp1 <121023220+wengzp1@users.noreply.github.com>
2025-04-24 10:26:19 +0800

    Update trainct.py

--- trainct.py
+++ trainct.py
@@ -15,7 +15,7 @@ from models import *
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', default='scaleupdehazing', type=str, help='model name')
+parser.add_argument('--model', default='scaleupdehazing-b', type=str, help='model name')
 parser.add_argument('--num_workers', default=16, type=int, help='number of workers')
 parser.add_argument('--no_autocast', action='store_false', default=True, help='disable autocast')
 parser.add_argument('--save_dir', default='./saved_models/', type=str, help='path to models saving')
