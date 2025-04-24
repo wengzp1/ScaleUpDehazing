14d9c42662ca6b623e8d105bd4d01cbcadf3e7a6
wengzp1 <121023220+wengzp1@users.noreply.github.com>
2025-04-24 10:27:11 +0800

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ from models import *
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', default='scaleupdehazing', type=str, help='model name')
+parser.add_argument('--model', default='scaleupdehazing-b', type=str, help='model name')
 parser.add_argument('--num_workers', default=16, type=int, help='number of workers')
 parser.add_argument('--data_dir', default='./data/', type=str, help='path to dataset')
 parser.add_argument('--save_dir', default='./saved_models/', type=str, help='path to models saving')
